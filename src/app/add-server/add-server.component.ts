5e8fe8c537492a5997ce35247537e18cc06cb9b3
sparshturkane <rajaturkane99@gmail.com>
2017-09-05 19:53:45 +0530

    68 using local references in templates

--- src/app/add-server/add-server.component.ts
+++ src/app/add-server/add-server.component.ts
@@ -8,21 +8,21 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 export class AddServerComponent implements OnInit {
     @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
     @Output() blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
-    newServerName = '';
+    // newServerName = '';
     newServerContent = '';
     constructor() {}
 
     ngOnInit() {}
-    onAddServer() {
+    onAddServer(serverNameInput: HTMLInputElement) {
         this.serverCreated.emit({
-            serverName: this.newServerName,
+            serverName: serverNameInput.value,
             serverContent: this.newServerContent
         })
     }
 
-    onAddBlueprint() {
+    onAddBlueprint(serverNameInput: HTMLInputElement) {
         this.blueprintCreated.emit({
-            serverName: this.newServerName,
+            serverName: serverNameInput.value,
             serverContent: this.newServerContent
         })
     }
