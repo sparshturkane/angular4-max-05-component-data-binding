cea3a6815ff521f1d8ef6b0e577923fe5e9bee1a
sparshturkane <rajaturkane99@gmail.com>
2017-09-05 19:18:48 +0530

    63 binding to custom events

--- src/app/add-server/add-server.component.ts
+++ src/app/add-server/add-server.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
     selector: 'app-add-server',
@@ -6,26 +6,25 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./add-server.component.css']
 })
 export class AddServerComponent implements OnInit {
+    @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+    @Output() blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
     newServerName = '';
     newServerContent = '';
     constructor() {}
 
     ngOnInit() {}
     onAddServer() {
-        // this.serverElements.push({
-        //     type: 'server',
-        //     name: this.newServerName,
-        //     content: this.newServerContent
-        // });
+        this.serverCreated.emit({
+            serverName: this.newServerName,
+            serverContent: this.newServerContent
+        })
     }
 
     onAddBlueprint() {
-        // this.serverElements.push({
-        //     type: 'blueprint',
-        //     name: this.newServerName,
-        //     content: this.newServerContent
-        // });
+        this.blueprintCreated.emit({
+            serverName: this.newServerName,
+            serverContent: this.newServerContent
+        })
     }
 
 }
-
